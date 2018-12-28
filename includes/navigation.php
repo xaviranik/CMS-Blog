e5d3a56b4b41d923d794d0ed7d779f8675e4140e
Xavir <xaviranik.me@gmail.com>
2018-12-28 20:11:08 +0600

    Added: Category Page

--- includes/navigation.php
+++ includes/navigation.php
@@ -33,7 +33,7 @@
 
                         while ($row = mysqli_fetch_assoc($query)) {
                             $cat_title = $row['cat_title'];
-                            echo "<li><a href='search.php?search={$cat_title}&submit=done'>{$cat_title}</a></li>";
+                            echo "<li><a href='category.php?category={$cat_title}&submit=done'>{$cat_title}</a></li>";
                         }
                         ?>
                         </ul>
